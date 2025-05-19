9edb143d7cbc78057b38d4e366f46d29af0e4420
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-05-20 01:07:16 +0800

    chore: update favicon, title, and meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,9 @@ import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/c
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'Frontend Mentor | Product feedback app',
+  description:
+    'A full-stack product feedback app built with Next.js. Users can submit, upvote, and comment on feature requests to help prioritize product development.',
 };
 
 export default function RootLayout({ children }: { children: any }) {
@@ -15,7 +16,6 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en" {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
