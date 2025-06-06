c5a831b1ff26fedf57e97784676932c12c89c467
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-06-06 10:04:27 +0800

    feat: implement nav drawer in mobile breakpoint

--- components/SuggestionsNav/SuggestionsNav.tsx
+++ components/SuggestionsNav/SuggestionsNav.tsx
@@ -1,6 +1,11 @@
+'use client';
+
+import { Burger, Drawer } from '@mantine/core';
+import { useDisclosure, useViewportSize } from '@mantine/hooks';
 import { CategoryFilter } from '../CategoryFilter';
 import { Roadmap } from '../Roadmap';
 import { TitleCard } from '../TitleCard';
+import classes from './SuggestionsNav.module.css';
 
 interface SuggestionsNavProps {
   categories: { id: number; name: string }[];
@@ -8,11 +13,34 @@ interface SuggestionsNavProps {
 }
 
 export function SuggestionsNav({ categories, statusesWithCount }: SuggestionsNavProps) {
+  const [opened, { close, toggle }] = useDisclosure(false);
+  const { width } = useViewportSize();
+
   return (
     <>
-      <TitleCard />
-      <CategoryFilter categories={categories} />
-      <Roadmap statusesWithCount={statusesWithCount} />
+      <TitleCard
+        drawerToggle={
+          <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" hiddenFrom="sm" />
+        }
+      />
+      {width < 768 ? (
+        <Drawer
+          opened={opened}
+          onClose={close}
+          title="Navigation"
+          position="right"
+          withCloseButton={false}
+          classNames={{ inner: classes.inner, overlay: classes.overlay }}
+        >
+          <CategoryFilter categories={categories} />
+          <Roadmap statusesWithCount={statusesWithCount} />
+        </Drawer>
+      ) : (
+        <>
+          <CategoryFilter categories={categories} />
+          <Roadmap statusesWithCount={statusesWithCount} />
+        </>
+      )}
     </>
   );
 }
--- components/TitleCard/TitleCard.tsx
+++ components/TitleCard/TitleCard.tsx
@@ -1,12 +1,18 @@
+import { ReactNode } from 'react';
 import classes from './TitleCard.module.css';
 
-export function TitleCard() {
+interface TitleCardProps {
+  drawerToggle?: ReactNode;
+}
+
+export function TitleCard({ drawerToggle }: TitleCardProps) {
   return (
     <div className={classes.card}>
       <div className={classes.text}>
         <p>Frontend Mentor</p>
         <h1>Feedback Board</h1>
       </div>
+      {drawerToggle}
     </div>
   );
 }
